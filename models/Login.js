dd8142923db17052c4c0d13a76355bf912969e77
anjanisonali <anjanibendre260201@gmail.com>
2020-09-24 23:07:35 +0700

    model/Login.js

--- models/Login.js
+++ models/Login.js
@@ -2,6 +2,8 @@ const mongoose = require('mongoose');
 const  {
     Schema
 } = mongoose;
+//ini baru di oush
+//ini dari gitu
 
 const loginuserSchema = new Schema({
     email : String,
@@ -10,3 +12,5 @@ const loginuserSchema = new Schema({
 
 const Login = mongoose.model('Login', loginuserSchema);
 module.exports = Login;
+//wkw kwkwkwkw
+//skrg kamu coba oush yang ini
