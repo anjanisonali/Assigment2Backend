e3ad062bd0015c03d459a34ca79e667d4a98eb88
anjanisonali <anjanibendre260201@gmail.com>
2020-09-30 21:03:13 +0700

    models/User.js

--- models/User.js
+++ models/User.js
@@ -1,40 +1,23 @@
 const mongoose = require('mongoose');
 const bcrypt = require('bcrypt');
-const Townhall = require('../models/Townhall')
-const Power = require('../models/Power')
-const{
-    Schema
-}= mongoose;
 
-// Schema
-
-const userSchema = new Schema({
-    username : {
-        type: String,
-    },
-    email : {
-        type: String,
-        default : 'example@me.com'
-    },
-    password : {type: String, max: 7},
-
-
-townhalls: [{
-    type : Schema.Types.ObjectId,
-    ref: 'Power'
-}]
-
+const userSchema = new mongoose.Schema({
+  email: { type: String, required: true },
+  password: { type: String, required: true },
 });
 
-userSchema.pre('save', async function (next){
-    console.log(this.username, 'Oke running')
-    const salt = await bcrypt.genSalt(3)
-    const hash = await bcrypt.hash(this.password, salt)
-    this.password = hash
-
-    console.log(this.password)
-    next()
-})
-
+userSchema.pre('save', function (next) {
+  User.findOne({ email: this.email })
+    .then((user) => {
+      if (user) next({ name: "ALREADY_EXISTS" });
+      else {
+        const salt = bcrypt.genSaltSync(10);
+        this.password = bcrypt.hashSync(this.password, salt);
+        next();
+      }
+    })
+    .catch((err) => next({ name: "MONGOOSE_ERROR" }));
+});
 const User = mongoose.model('User', userSchema);
+
 module.exports = User;
